293e462d6fbb9c3338505111fb5836712146bb8f
sol <ocosio0907@gmail.com>
2023-03-24 19:56:24 +0900

    refactoring and fix footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,12 @@
 </script>
 
 <div class="app">
-	<main>
+	<main class="flex w-screen h-screen justify-center items-center">
 		<slot />
 	</main>
+	<footer class="flex w-screen justify-center items-center">
+		<p class="opacity-70">made by p1nguuuuu</p>
+	</footer>
 </div>
 
 <style>
