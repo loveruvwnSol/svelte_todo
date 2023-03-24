9864d40d9506a5464e7be3876ec5142c328582dc
sol <ocosio0907@gmail.com>
2023-03-24 20:46:42 +0900

    add textbox and todoList

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,78 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+<script lang="ts">
+	type Todo = {
+		id: number;
+		title: string;
+		status: string;
+	};
+	let value = '';
+	let count = 1;
+	let array: Todo[] = [];
+
+	function pushValue() {
+		if (!value.match(/\S/g)) {
+			alert('テキストを入力してください！');
+		} else {
+			array.push({ id: count, title: value, status: '' });
+			array = array;
+			value = '';
+			count += 1;
+			console.log(array);
+		}
+	}
 </script>
 
 <svelte:head>
-	<title>Home</title>
+	<title>svelte todo app</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div>
+	<div class="form-control">
+		<div class="input-group">
+			<input
+				type="text"
+				placeholder="タイトル"
+				class="input input-bordered w-96 font-bold"
+				bind:value
+			/>
+			<button class="btn btn-square w-20" on:click={pushValue}> 追加 </button>
+		</div>
+	</div>
+	<div class="overflow-x-auto mt-10">
+		<table class="table w-full">
+			<thead>
+				<tr>
+					<td />
+					<th>タイトル</th>
+					<th>ステータス</th>
+				</tr>
+			</thead>
+			<tbody>
+				{#each array as todo}
+					<tr>
+						<th>{todo.id}</th>
+						<td>{todo.title}</td>
+						<td>
+							<button
+								class="btn btn-active bg-white border-0 shadow-xl w-14 hover:bg-white focus:bg-slate-700"
+								on:click={() => (todo.status = '😎')}>😎</button
+							>
+							<button
+								class="btn btn-active bg-white border-0 shadow-xl w-14 hover:bg-white focus:bg-slate-700"
+								on:click={() => (todo.status = '😇')}>😇</button
+							>
+							<button
+								class="btn btn-active bg-white border-0 shadow-xl w-14 hover:bg-white focus:bg-slate-700"
+								on:click={() => (todo.status = '😅')}>😅</button
+							>
+							<button
+								class="btn btn-active bg-white border-0 shadow-xl w-14 hover:bg-white focus:bg-slate-700"
+								on:click={() => (todo.status = '😡')}>😡</button
+							></td
+						>
+					</tr>
+				{/each}
+			</tbody>
+		</table>
+	</div>
+</div>
